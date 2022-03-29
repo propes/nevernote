87ba92cc0070a52ecf37b5d46dc6731932650222
james.propert <james.propert@drdoctor.co.uk>
2022-03-29 20:47:11 +0100

    Add whitespace styles

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import "./styles/whitespace.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
